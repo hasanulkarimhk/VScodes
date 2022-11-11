da0f9b9cc6eccec81a4a0652de5395a9d39ccb7f
Hasanul <hasanulkarimhk@yahoo.com>
2022-11-12 03:55:00 +0600

    lovecoding

--- trial.c
+++ trial.c
@@ -1,22 +1,27 @@
-#include <stdio.h>
+#include<stdio.h>
 
 void main()
 {
-    int i=1, num;
+    int num, i, i2, num_rev=0;
 
-    printf("Enter any number to find its factor: ");
+    printf("Enter a number:");
     scanf("%d", &num);
+    i=num%10;
+    printf("last digit is %d\n", i);
 
-    while(i<num){
-            if(num%i==0){
-                printf("%d\n", i);
-            }
-            i++;
+    while(num!=0)
+    {
+        num_rev = (num_rev*10)+ (num%10);
+
+        num/=10;
+
+
+
+    }
+    i2=num_rev%10;
+    printf("first digit is %d", i2);
 
 
 
 
     }
-
-
-}
